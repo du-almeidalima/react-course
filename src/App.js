28389643e2152d0f2150c294f181c78825a9dea7
Xenosgrilda <dualmeidalima@gmail.com>
2020-08-18 22:21:00 -0300

    Section 3: Working with Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Hello React World
+        <Person name={'Eduardo'} age={23} />
+        <Person name={'Jolie'} age={25} />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = (props) => {
+  return (
+      <h1>Hey! I'm { props.name } and I have { props.age } years old!</h1>
+  )
+};
+
+export default person;
