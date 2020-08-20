0778edcdbb9ed0efe16f1b903820628775344724
Xenosgrilda <dualmeidalima@gmail.com>
2020-08-19 23:06:03 -0300

    Section 3: Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
-    people: [
-      { name: 'Eduardo', age: 23},
-      { name: 'Jolie', age: 25},
-      { name: 'Juaum', age: 18}
-    ],
-    hobby: 'I really like watching Netflix'
+const App = props => {
+
+  const [hobby, setHobbyState] = useState('I really like watching Netflix');
+  const [people, setPeopleState] = useState([
+    {name: 'Eduardo', age: 23},
+    {name: 'Jolie', age: 25},
+    {name: 'Juaum', age: 18}
+  ]);
+
+  const switchNameHandler = () => {
+    setPeopleState([people.pop(), ...people])
   }
 
-  switchNameHandler = () => {
-    const people = [
-      this.state.people.pop(),
-      ...this.state.people
-    ]
-
-    this.setState({ people })
-  }
-
-  render() {
-    return (
+  return (
       <div className="App">
         <h1>Hello React World</h1>
-        <button onClick={ this.switchNameHandler }>Switch Names</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
-        <Person name={this.state.people[1].name} age={this.state.people[1].age} />
-        <Person name={this.state.people[2].name} age={this.state.people[2].age} >{this.state.hobby}</Person>
+        <button onClick={switchNameHandler}>Switch Names</button>
+        <Person name={people[0].name} age={people[0].age}/>
+        <Person name={people[1].name} age={people[1].age}/>
+        <Person name={people[2].name} age={people[2].age}>{hobby}</Person>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
 
 /*
- * setState will take an object and merge it with the current state, only overriding the given properties
+ * React hooks, such as useState, are basically functions that let you add functionalities to functional components.
+ * useState returns the stateObject and a function to update it. And you can make multiple calls to this hook for each
+ * state.
  */
