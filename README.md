0384643f074b58a6d1d852c62f83770f6937210f
Xenosgrilda <dualmeidalima@gmail.com>
2020-08-22 00:46:12 -0300

    Section 4: Rendering Content Conditionally

--- README.md
+++
@@ -1,5 +0,0 @@
-# Assignment 1: React Basics
-
-This project is the first assignment of this course to practice the React Hello World.
-
-![Assignment-Picture](./assignment.png)
--- src/App.js
+++ src/App.js
@@ -1,40 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import UserOutput from "./UserOutput/UserOutput";
-import UserInput from "./UserInput/UserInput";
+import Person from "./Person/Person";
 
 class App extends Component {
+
   state = {
-    name: 'Alfredo'
+    people: [
+      {name: 'Eduardo', age: 23},
+      {name: 'Jolie', age: 25},
+      {name: 'Juaum', age: 18}
+    ],
+    hobby: 'I really like watching Netflix',
+    showPeople: false
   }
 
-  nameInputHandler = (e) => {
-    this.setState({ name: e.target.value })
+  togglePeopleHandler = () => {
+    this.setState({showPeople: !this.state.showPeople})
+  }
+
+  inputHandler = (e) => {
+    const newName = e.target.value;
+    const person = this.state.people[0]
+    person.name = newName;
+
+    this.setState({people: [person, ...this.state.people.slice(1)]})
   }
 
   render() {
-    return (
-      <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
-          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-        </ol>
+    const style = {
+      background: 'lightblue',
+      fontWeight: 'bold',
+      padding: '10px 5px',
+      border: '1px solid black',
+      cursor: 'pointer'
+    }
 
-        <section className="App-User">
-          <UserInput inputHandler={this.nameInputHandler} value={this.state.name}/>
-          <UserOutput name={this.state.name}/>
-        </section>
-      </div>
-    );
+    return (
+        <div className="App">
+          <h1>Hello React World</h1>
+          <button style={style} onClick={this.togglePeopleHandler}>Toggle People</button>
+          {
+            this.state.showPeople
+                ?
+                <div>
+                  <Person name={this.state.people[0].name} age={this.state.people[0].age} input={this.inputHandler}/>
+                  <Person name={this.state.people[1].name} age={this.state.people[1].age}/>
+                  <Person name={this.state.people[2].name} age={this.state.people[2].age}>
+                    {this.state.hobby}
+                  </Person>
+                </div>
+                : null
+          }
+        </div>
+    )
   }
 }
 
 export default App;
+
+/*
+ * There is two ways of passing a function with parameters that are going to be executed in other component
+ *  - bind(this, ...args)
+ *  - arrow function: () => { this.customFunction('parameter') }
+ */
