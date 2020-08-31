c67436d6dd076eb6f590835999c968943e62edfd
Eduardo de Almeida Lima <eduardo.lima@venturus.org.br>
2020-08-31 15:34:35 -0300

    Section 7: Using and Tweaking useEffect Hook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import cockpitStyle from './Cockpit.css';
 
 const cockpit = (props) => {
+
+  // Triggers when props.showPeople change
+  useEffect(() => {
+    console.log('[Cockpit] >> useEffect');
+
+    // Httprequest
+    setTimeout(() => {
+      alert('Some async work has been done!')
+    }, 1000);
+
+  }, [props.showPeople])
+
+  // Triggers only once, equivalent to componentDidMount
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('[Cockpit] >> useEffect as componentDidMount');
+    }, 1000);
+  }, [])
+
   return (
     <React.Fragment>
       <h1>Hello React World</h1>
@@ -18,3 +37,10 @@ const cockpit = (props) => {
 };
 
 export default cockpit;
+
+/**
+ * useEffect is kind of a all in one lifecycle hook, it executes on component creation, update and so on...
+ * But we can tweak it, of example we can set this function to only execute when a certain value/object change.
+ * 
+ * And we can set it / call it more than once
+ */
