0a569852e497b60af17b159985e5cdaeff4c527c
Eduardo de Almeida Lima <eduardo.lima@venturus.org.br>
2020-09-08 11:28:06 -0300

    Section 8: Burger App Skeleton

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import layoutStyle from "./Layout.module.css";
+
+const layout = (props) => (
+  <React.Fragment>
+    <header>
+      <ul className={layoutStyle.LayoutPagesList}>
+        <li className={layoutStyle.LayoutPagesItem}>Toolbar</li>
+        <li className={layoutStyle.LayoutPagesItem}>SideDrawer</li>
+        <li className={layoutStyle.LayoutPagesItem}>Backdrop</li>
+      </ul>
+    </header>
+
+    <main>{props.children}</main>
+  </React.Fragment>
+);
+
+export default layout;
