41d5249d3d104d0b7bfcd80cee8cde334ad5df9e
Xenosgrilda <dualmeidalima@gmail.com>
2020-09-13 01:46:10 -0300

    Section 8: Outputting Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,21 @@ import burgerStyle from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+
+  // This code creates an list of ingredients based on type and the amount
+  const ingredientsList = Object.entries(props.ingredients).reduce((acc, curr) => {
+    const repeatedIngredients = [...Array(curr[1])].map((_, i) => (
+        <BurgerIngredient key={curr[0] + i} type={curr[0]}/>
+    ))
+
+    return [...acc, ...repeatedIngredients];
+  }, [])
+
   return (
       <div className={burgerStyle.Burger}>
-        <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="meat"/>
-        <BurgerIngredient type="bread-bottom"/>
+        <BurgerIngredient type="bread-top" key='bread-top'/>
+        {ingredientsList}
+        <BurgerIngredient type="bread-bottom" key='bread-bottom'/>
       </div>
   )
 }
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -10,12 +10,14 @@ const createIngredientTemplate = ingredientType => {
   switch (ingredientType) {
     case 'bread-bottom':
       return <div className={burgerIngredientStyle.BreadBottom} />
+    case 'salad':
+      return <div className={burgerIngredientStyle.Salad} />
     case 'meat':
       return <div className={burgerIngredientStyle.Meat} />
     case 'cheese':
       return <div className={burgerIngredientStyle.Cheese} />
     case 'bacon':
-      return <div className={burgerIngredientStyle.Cheese} />
+      return <div className={burgerIngredientStyle.Bacon} />
     case 'bread-top':
       return (
           <div className={burgerIngredientStyle.BreadTop}>
