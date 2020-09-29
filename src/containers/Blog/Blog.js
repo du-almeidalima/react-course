0250fe37c241b2c49603423ca1f36cad23257422
Eduardo de Almeida Lima <eduardo.lima@venturus.org.br>
2020-09-29 16:04:53 -0300

    Section 11: Router Props and withRouter HOC

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import { Route } from "react-router";
+import { Link } from "react-router-dom";
+
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 import "./Blog.css";
 
@@ -12,19 +16,20 @@ class Blog extends Component {
             <nav className="Navbar">
                 <ul>
                     <li>
-                        <a href={'/'}>Home</a>
+                      <Link to="/">Home</Link>
                     </li>
                     <li>
-                        <a href={'/posts'}>Posts</a>
+                      <Link to={{ pathname: '/compose' }}>Compose</Link>
                     </li>
                     <li>
-                        <a href={'/compose'}>Compose</a>
+                      <Link to="/users">Users</Link>
                     </li>
                 </ul>
             </nav>
         </header>
-        
-        <Posts />
+
+        <Route path="/" exact component={Posts} />
+        <Route path="/compose" exact component={NewPost} />
       </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -11,7 +11,6 @@ export default class Posts extends Component {
     error: false,
   };
 
-
   componentDidMount() {
     PostAPI
       .get("/posts")
