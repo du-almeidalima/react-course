60a39a6501a411335e10989d2e056e9637a44d9c
Eduardo Lima <dualmeidalima@gmail.com>
2020-09-30 21:59:05 -0300

    Section 11: Lazy Loading Components with React.Lazy and Suspense 16.6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, {Component, Suspense} from 'react';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 
-import Blog from "./containers/Blog/Blog";
+// import Posts from './containers/Posts';
+// import User from './containers/User';
+import Welcome from './containers/Welcome';
+
+const User = React.lazy(() => import('./containers/User'));
+const Posts = React.lazy(() => import('./containers/Posts'));
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
-    );
-  }
+    state = {
+        loadPosts: false
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <React.Fragment>
+                    <nav>
+                        <NavLink to="/user">User Page</NavLink> |&nbsp;
+                        <NavLink to="/posts">Posts Page</NavLink>
+                    </nav>
+                    <Route path="/" component={Welcome} exact/>
+                    <Route path="/user" render={() => (
+                        <Suspense fallback={<div>Loading</div>}>
+                            <User/>
+                        </Suspense>
+                    )}/>
+
+                    {/*  Loading Components without Route */}
+                    <hr/>
+                    <button onClick={() => this.setState({loadPosts: true})}
+                            disabled={this.state.loadPosts}
+                    >
+                        Load Posts Component
+                    </button>
+
+                    { this.state.loadPosts ?
+                        (
+                            <Suspense fallback={<h1>Loading Posts...</h1>}>
+                                <Posts />
+                            </Suspense>
+                        )
+                        : null
+                    }
+                </React.Fragment>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
