49bede89327aa6f5e54326cc0c11b11c69f1226c
Eduardo de Almeida Lima <eduardo.lima@venturus.org.br>
2020-10-07 17:13:38 -0300

    Section 12: Outputting the Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,19 @@ import React from 'react';
 import OrderStyle from './Order.module.css';
 
 const Order = (props) => {
+  const ingredientsArr = Object.entries(props.ingredients);
+  const ingredientsTitleTemplate = ingredientsArr.map(([key, value], index) => {
+    return (
+      <span className={OrderStyle.OrderItem} key={key}>
+        {key}({value})
+      </span>
+    )
+  })
+
   return (
     <div className={OrderStyle.OrderWrapper}>
-      <span>Ingredients</span>
-      <span>Price: <strong>R$5.00</strong></span>
+      <div className={OrderStyle.OrderItemsWrapper}>{ingredientsTitleTemplate}</div>
+      <strong className={OrderStyle.Price}>R${props.price.toFixed(2)}</strong>
     </div>
   )
 }
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react'
-import Order from '../../components/Order/Order'
+import React, { Component } from "react";
+import Order from "../../components/Order/Order";
+import burgerBuilderAPI from "../../api/burger-builder.api";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
-export default class Orders extends Component {
+class Orders extends Component {
+  state = {
+    orders: [],
+    loading: false
+  }
+
+  componentDidMount() {
+    burgerBuilderAPI.get("/orders.json")
+      .then((res) => {
+        // FireBase return a Object with all the items, so we need to turn it into an array
+        const ordersArr = Object.entries(res.data)
+          .map(([key, value]) => {
+            return {
+              ...value,
+              id: key
+            }
+          });
+
+        this.setState({ orders: ordersArr });
+      })
+      .catch((err) => {
+        console.error("[Orders]: ", err);
+        this.setState({ error: true });
+      });
+  }
+
   render() {
     return (
       <div>
         <h1 className="PageTitle">Orders</h1>
-        <Order />
-        <Order />
-        <Order />
+        { this.state.orders.map(order => (
+          <Order key={order.id} ingredients={order.ingredients} price={+order.price}/>
+        ))}
       </div>
-    )
+    );
   }
 }
+
+export default withErrorHandler(Orders, burgerBuilderAPI);
