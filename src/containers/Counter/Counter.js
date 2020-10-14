a1a8cd32bfa385a8332dabf232e1e1c8fb49f852
Eduardo Lima <dualmeidalima@gmail.com>
2020-10-14 09:36:50 -0300

    Section 13: Connecting React with Redux and Store with React

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -28,7 +29,7 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +39,24 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+/*
+ * A Component that is using Redux, will no longer have an state, instead, its state is going to be provided by Redux.
+ * But remember that Redux provide this is state by wrapping the Component into a HOC, so the state will be getting as
+ * props. This function is how you can map the whole application state into props that this component will receive.
+ */
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    }
+}
+
+export default connect(mapStateToProps)(Counter);
+
+/* To connect Redux with a Component, we need to wrap it into Redux, so it can access the State and other Redux props.
+ * This can be done with the react-redux function "connect()".
+ * This function works like a HOC, the difference is that, it's not a HOC but itself, but it returns a HOC. And in this
+ * HOC we can wrap the Component.
+ *
+ * Connect accepts some parameters, some of them are the part of the state you are interested and the Actions this
+ * component can dispatch.
+ */
---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type){
+
+    default:
+      return state;
+  }
+}
+
+export default reducer;
