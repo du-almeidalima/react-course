e84ea865c864a0750bac65c022254977a390a189
Eduardo Lima <dualmeidalima@gmail.com>
2020-10-16 10:37:08 -0300

    Section 17: Redirect User on Checkout Refresh

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,28 +1,33 @@
 import React, {Component} from 'react';
-import { connect } from "react-redux";
-import { Route } from "react-router-dom";
+import {connect} from "react-redux";
+import {Route, Redirect} from "react-router-dom";
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import Contact from './Contact/Contact';
 
 class Checkout extends Component {
+  render() {
+    let content = <Redirect to="/"/>;
 
-    render() {
-        return (
-            <div>
-                <h1 className="PageTitle">Checkout</h1>
-                <CheckoutSummary ingredients={this.props.ingredients} />
-                <Route path={this.props.match.path + '/contact'} component={Contact} />
-            </div>
-        );
+    if (Object.keys(this.props.ingredients).length > 0) {
+      content = (
+          <div>
+            <h1 className="PageTitle">Checkout</h1>
+            <CheckoutSummary ingredients={this.props.ingredients}/>
+            <Route path={this.props.match.path + '/contact'} component={Contact}/>
+          </div>
+      )
     }
+
+    return content;
+  }
 }
 
 // == REDUX ==
 const mapStateToProps = state => {
-    return {
-        ingredients: state.burgerBuilder.ingredients
-    }
+  return {
+    ingredients: state.burgerBuilder.ingredients
+  }
 }
 
 export default connect(mapStateToProps)(Checkout);
